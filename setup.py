71ffd869a6473b854a6b8ba7979097a025cf1a51
P-Victor <irap4fun@gmail.com>
2014-05-30 23:06:28 +0000

    tweaked setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ except ImportError:
 import sys
 
 config = {
-    'name' : 'ServiceDeskAid',
-    'include_package_data' : True,
-    'zip_safe' : False,
-    'packages' : s.find_packages(),
-    'install_requires': [
+    'name': 'ServiceDeskAid',
+    'include_package_data': True,
+    'version': '0.1.0',
+    'author': 'thecodeflavour.org',
+    'url': 'http://codeflavour.github.io/servicedeskaid/',
+    'long_description' : open('README.md').read()
+    'zip_safe': False,
+    'packages': s.find_packages(),
+    'install_requires':[
         'flask',
         'flask-restful',
         'flask-mongoalchemy',
