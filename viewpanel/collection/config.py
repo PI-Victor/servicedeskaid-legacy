e3f8ff6261fade86cdc31bde4393e64da8c3bcb4
PI-Victor <irap4fun@gmail.com>
2014-11-10 20:08:08 +0100

    config for menu in place

---
+++ viewpanel/collection/config.py
@@ -0,0 +1,23 @@
+'''Config for the main menu
+'''
+
+main_menu = { "Live Feed": { 1 : 'View open',
+                             2: 'Recently closed',
+                         }
+             "Users": { 1: "View Users",
+                        2: "Wall",
+             }
+             "Statistics": {1: "View",
+                            2: "Customize",
+             }
+             "Time Zones": {1: "View",
+                            2: "Customize",
+             }
+}
+
+login_menu = { "Login": { 1: "Login",
+                          2: "Customize",
+                          3: "logout",
+    
+                      }
+}
