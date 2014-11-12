c99dfb04fff24e7d85443cc2b258446d179072b0
PI-Victor <irap4fun@gmail.com>
2014-11-13 00:38:33 +0100

    refactored login, added form validation

--- viewpanel/collection/forms.py
+++ viewpanel/collection/forms.py
@@ -1,8 +1,13 @@
-from wtforms import Form, BooleanField, TextField, validators
+from wtforms import Form, BooleanField, TextField, PasswordField, validators
 from models import Users
 
 
 class LoginForm(Form):
+    loginname = TextField('Username', [validators.Length(min=4, max=25)])
+    loginpass = PasswordField('New Password', [validators.Required()])
+#    remember = BooleanField('Remember me'), [validators.Required()]
+    
+class RegisterForm(Form):
     username = TextField('Username'), [validators.Length(min=4, max=25)]
     password = TextField('Password'), [validators.Length(min=6, max=25)]
     email = TextField('Email Address'), [validators.Length(min=6, max=35)]
--- viewpanel/collection/views.py
+++ viewpanel/collection/views.py
@@ -1,44 +1,49 @@
-import flask
+from flask import g, request, render_template, flash, abort, redirect
 from viewpanel import pages
 from viewpanel.collection.models import Users 
 from forms import LoginForm
 
+
+@pages.before_request
+def get_user():
+    g.user = 'test'
+
 @pages.route('/login', methods=['GET', 'POST'])
 def user_login():
-    form = LoginForm()
-    if flask.request.method == 'POST' and form.validate():
-        print form.username.data
-        user = Users.objects(form.username.data, form.password.data).get()
-        print user
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = Users.objects(userid=form.loginname.data, 
+                             password=form.loginpass.data).get()
         if user is not None:
-            print "I got here for some reason"
-            return flask.render_template('wall.html')
-    elif flask.request.method == 'GET':
-        return flask.render_template('login.html', error=True)
+            g.user = user
+            return redirect('viewpanel')
+    elif request.method == 'GET':
+        return render_template('login.html', error='')
     else:
-        print "You screwed up"
+        error = "Invalid username or password"
+        return render_template('login.html', error=error)
 
 @pages.route('/')
 def index():
-    return flask.render_template('wall.html')
+    return render_template('wall.html')
 
 
 @pages.route('/viewpanel')
 def viewpanel():
-    return flask.render_template('viewpanel.html')
+    return render_template('viewpanel.html')
 
 
 @pages.route('/timezones')
 def timezones():
-    return flask.render_template('timezones.html')
+    return render_template('timezones.html')
 
 
 @pages.route('/users')
 def show_users():
 
-    return flask.render_template('users.html', Users=Users)
+    return render_template('users.html', Users=Users)
 
 
 @pages.route('/statistic')
 def statistic():
-    return flask.render_template('statistic.html')
+    return render_template('statistic.html')
