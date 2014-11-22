05b196be6c28a1adc0aa06f32b476ab8f02340df
PI-Victor <palade.ionut@gmail.com>
2014-11-22 16:33:14 +0100

    changed form valid for remember me field

--- viewpanel/collection/forms.py
+++ viewpanel/collection/forms.py
@@ -5,12 +5,9 @@ from models import Users
 class LoginForm(Form):
     loginname = TextField('Username', [validators.Length(min=4, max=25)])
     loginpass = PasswordField('New Password', [validators.Required()])
-#    remember = BooleanField('Remember me'), [validators.Required()]
+    remember = BooleanField('Remember me') 
     
 class RegisterForm(Form):
     username = TextField('Username'), [validators.Length(min=4, max=25)]
     password = TextField('Password'), [validators.Length(min=6, max=25)]
     email = TextField('Email Address'), [validators.Length(min=6, max=35)]
-    remember = BooleanField('Remember me'), [validators.Required()]
-    
-    
