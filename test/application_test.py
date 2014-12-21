ab4633fb9afa04813b8e432ab3282f888989c04c
PI-Victor <palade.ionut@gmail.com>
2014-12-21 20:35:24 +0100

    refactored tests and cleaned up the code

--- test/application_test.py
+++ test/application_test.py
@@ -1,10 +1,18 @@
-from viewpanel import app_factory
-from flask import Flask
+'''Test application and database settings
+'''
 
+from flask import Flask
+from viewpanel import db_factory
+from viewpanel import app_factory
 
 def test_app():
     app = app_factory()
     assert isinstance(app, Flask)
+
+
+def test_db():
+    db = db_factory()
+    assert db is not None
 
 
 
--- test/mongo_test.py
+++ test/mongo_test.py
@@ -1,42 +1,24 @@
-from viewpanel.collection.models import Users, Issues, Metrics
-import datetime as dt
-import random
-
-from viewpanel import db_factory
-
-"""Test insertion into the DB, save document, assert it's the document
+'''Test insertion into the DB, save document, assert it's the document
 inserted upon select and then delete the entry to avoid unwanted entries
-"""
-
-def test_db():
-    db = db_factory()
-    assert db is not None
+'''
 
 
-long_comment = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem " \
-               "accusantium doloremque laudantium, totam rem aperiam, eaque ipsa " \
-               "quae ab illo inventore veritatis et quasi architecto beatae vitae " \
-               "dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit " \
-               "aspernatur aut odit aut fugit, sed quia consequuntur magni dolores " \
-               "eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, " \
-               "qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, " \
-               "sed quia non numquam eius modi tempora incidunt ut labore et dolore " \
-               "magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis " \
-               "nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut " \
-               "aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit " \
-               "qui in ea voluptate velit esse quam nihil molestiae consequatur, " \
-               "vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+import random
+import datetime as dt
+from viewpanel.collection.models import Users, Issues, Metrics
+
 
 
 def test_user_insert():
     user_rand = 'testuser_{}'.format(str(random.random()))
-    users = Users(userid=user_rand,
-                  email='victor@scifi.thecodeflavour.org',
-                  fullname='Administrator',
-                  password='123123',
-                  role = 'admin',
-                  full_name = 'test1',
-              )
+    users = Users(
+        userid=user_rand,
+        email='victor@scifi.thecodeflavour.org',
+        fullname='Administrator',
+        password='123123',
+        role = 'admin',
+        full_name = 'test1',
+    )
     users.save()
     db_user = Users.objects(userid=user_rand).get()
     assert db_user.get_id() == users.get_id()
@@ -44,9 +26,11 @@ def test_user_insert():
 
 
 def test_metrics_insert():
-    metrics = Metrics(worked_issues=155055,
-                      open_issues=3,
-                      closed_issues=4)
+    metrics = Metrics(
+        worked_issues=155055,
+        open_issues=3,
+        closed_issues=4,
+    )
     metrics.save()
     resultset = Metrics.objects(worked_issues=155055).first()
     assert resultset.get_dataseries() == metrics.get_dataseries()
@@ -54,6 +38,21 @@ def test_metrics_insert():
 
 
 #def test_issues_insert():
+#long_comment = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem " \
+#               "accusantium doloremque laudantium, totam rem aperiam, eaque ipsa " \
+#               "quae ab illo inventore veritatis et quasi architecto beatae vitae " \
+#               "dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit " \
+#               "aspernatur aut odit aut fugit, sed quia consequuntur magni dolores " \
+#               "eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, " \
+#               "qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, " \
+#               "sed quia non numquam eius modi tempora incidunt ut labore et dolore " \
+#               "magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis " \
+#               "nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut " \
+#               "aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit " \
+#               "qui in ea voluptate velit esse quam nihil molestiae consequatur, " \
+#               "vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+
+
 #    comments = Issues.Comments(comment=long_comment)
 #    issues = Issues(comments=comments)
 #    issues.save()
