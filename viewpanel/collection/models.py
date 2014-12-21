98b3d409146f5bf4106c0768faf33d25308586ef
PI-Victor <palade.ionut@gmail.com>
2014-12-21 17:06:55 +0100

    fixed list definition for user roles and improved layout

--- viewpanel/collection/models.py
+++ viewpanel/collection/models.py
@@ -6,16 +6,28 @@ from viewpanel import db
 
 class Users(db.Document):
     ''' User information document'''
-    user_roles = (('admin',1), ('user',2), ('reader',3))
+    user_roles = (
+        ('admin',1),
+        ('user',2),
+        ('reader',3),
+    )
     email= db.EmailField(required=True)
     password = db.StringField(required=True)  
-    role = db.StringField(choices=roles, required=True, 
-                          default=roles[2])
+    role = db.StringField(
+        choices=user_roles,
+        required=True, 
+        default=user_roles[2],
+    )
     fullname = db.StringField(required=True)
     avatar = db.ImageField(size=(200, 200, True))
-    created = db.DateTimeField(required=True, 
-                               default=dt.datetime.utcnow())
-    userid = db.StringField(required=True, unique=True)
+    created = db.DateTimeField(
+        required=True, 
+        default=dt.datetime.utcnow()
+    )
+    userid = db.StringField(
+        required=True,
+        unique=True,
+    )
     
     def get_id(self):
         return self.userid
@@ -28,11 +40,22 @@ class Users(db.Document):
 
 class Metrics(db.Document):
     '''User and system performance metrics'''
-    timestamp = db.DateTimeField(required=True, 
-                                 default=dt.datetime.utcnow())
-    open_issues = db.IntField(min_value=0, required=True)
-    closed_issues = db.IntField(min_value=0, required=True)
-    worked_issues = db.IntField(min_value=0, required=True)
+    timestamp = db.DateTimeField(
+        required=True, 
+        default=dt.datetime.utcnow(),
+    )
+    open_issues = db.IntField(
+        min_value=0,
+        required=True,
+    )
+    closed_issues = db.IntField(
+        min_value=0,
+        required=True,
+    )
+    worked_issues = db.IntField(
+        min_value=0,
+        required=True,
+    )
 
 
     def get_dataseries(self):
@@ -52,17 +75,26 @@ class Issues(db.Document):
     '''Issue information collection'''
     class Comments(db.EmbeddedDocument):
         '''Comments embedded doc'''
-        posted = db.DateTimeField(required=True, 
-                                   default=dt.datetime.utcnow())
+        posted = db.DateTimeField(
+            required=True, 
+            default=dt.datetime.utcnow(),
+        )
         userid = db.StringField(required=True)
         user_name = db.StringField(required=True)
         text = db.StringField(required=True)
-
-    created = db.DateTimeField(required=True, 
-                               default=dt.datetime.utcnow())
+    created = db.DateTimeField(
+        required=True, 
+        default=dt.datetime.utcnow(),
+    )
     owner = db.StringField(required=True)
-    status = db.StringField(required=True, default='Open')
-    severity = db.StringField(required=True, default='Low')
+    status = db.StringField(
+        required=True,
+        default='Open',
+    )
+    severity = db.StringField(
+        required=True,
+        default='Low',
+    )
     last_updated = db.ListField(db.DateTimeField())
     closed = db.DateTimeField()
     comments = db.EmbeddedDocumentField(Comments)
