6a1e47fa164b2156a80e353bcbd508f776e56a48
PI-Victor <palade.ionut@gmail.com>
2014-12-30 12:50:02 +0100

    word wrapping in comments fixed

--- test/mongo_test.py
+++ test/mongo_test.py
@@ -51,19 +51,19 @@ def test_metrics_insert():
 
 
 def test_issues_insert():
-    long_comment = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem " \
-                   "accusantium doloremque laudantium, totam rem aperiam, eaque ipsa " \
-                   "quae ab illo inventore veritatis et quasi architecto beatae vitae " \
-                   "dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit " \
-                   "aspernatur aut odit aut fugit, sed quia consequuntur magni dolores " \
-                   "eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, " \
-                   "qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, " \
-                   "sed quia non numquam eius modi tempora incidunt ut labore et dolore " \
-                   "magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis " \
-                   "nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut " \
-                   "aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit " \
-                   "qui in ea voluptate velit esse quam nihil molestiae consequatur, " \
-                   "vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    long_comment = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem \
+    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa \
+    quae ab illo inventore veritatis et quasi architecto beatae vitae \
+    dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit \
+    aspernatur aut odit aut fugit, sed quia consequuntur magni dolores \
+    eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, \
+    qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, \
+    sed quia non numquam eius modi tempora incidunt ut labore et dolore  \
+    magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis \
+    nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut \
+    aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit \
+    qui in ea voluptate velit esse quam nihil molestiae consequatur, \
+    vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
     comment1 = Issues.Comments(
         userid = 'Victor123',
         fullname = 'Victor P',
@@ -95,17 +95,15 @@ def customer_test():
         email = 'joan@testcase.com',
         address = 'St Joan Rd. no 2, TX',
         contractno = '239045798345',
-        otherinfo = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem  \
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa  \
-        quae ab illo inventore veritatis et quasi architecto beatae vitae  \
-        dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit  \
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores  \
-        eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,  \
-        qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, ',
+        otherinfo = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem \
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa \
+        quae ab illo inventore veritatis et quasi architecto beatae vitae \
+        dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit \
+        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores \
+        eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, \
+        qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, "
     )
     customer.save()
     customer_result = Customer.objects(customerid=customer.customerid).first()
     assert customer_result.customerid == customer.customerid
     assert customer_result.telephone == customer.telephone
-                   
-    
