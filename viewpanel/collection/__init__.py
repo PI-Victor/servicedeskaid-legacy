90c30ff40e76afccb7ece77682dc9e623a236a90
PI-Victor <irap4fun@gmail.com>
2015-02-11 16:44:38 +0000

    added temporary fix for app ip binding

--- viewpanel/collection/__init__.py
+++ viewpanel/collection/__init__.py
@@ -1,10 +1,11 @@
 import os
 from . import *
-
+import socket
 
 #check to see if we are running a linked container mongodb 
 ckdbport = os.getenv("MONGODB_PORT_27017_TCP_PORT")
 ckdbhost = os.getenv("MONGODB_PORT_27017_TCP_ADDR")
+ckbindip = os.getenv("BINDIP") 
 #environment paths
 WORKDIR = os.path.join(os.path.sep,
                       os.path.dirname(os.path.realpath(__file__)))
@@ -13,7 +14,13 @@ LOGDIR = os.path.join(os.path.sep, WORKDIR, 'log')
 #mongodb settings
 DBNAME = 'deskdb'
 SECRETKEY = 't3st Patience'   #this should easy to configure 
-DEFAULTHOST = '127.0.0.1'
+
+#use a temporary workaround for docker container binding
+if ckbindip is None:
+    DEFAULTHOST = ([(s.connect(('8.8.8.8', 80)), s.getsockname()[0],
+                     s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
+else:
+    DEFAULTHOST = ckbindip
 
 if ckdbhost is None:
     DBHOST = '127.0.0.1'
