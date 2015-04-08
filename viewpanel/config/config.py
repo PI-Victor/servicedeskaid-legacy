f15c76e1abffd385d8bb537afc63ff0a748e02e2
PI-Victor <irap4fun@gmail.com>
2015-04-08 03:08:03 +0200

    added basic docker structure

--- viewpanel/config/config.py
+++ viewpanel/config/config.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s %(message)s',
 
 class Config(object):
     DIALECT = 'postgres'
-    WORKDIR =  os.path.join(os.path.sep, os.path.dirname(os.path.realpath(__file__)))
+    WORKDIR =  os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
     TMPDIR = os.path.join(os.path.sep, WORKDIR, 'tmp')
     LOGDIR = os.path.join(os.path.sep, WORKDIR, 'log')
     DATABASE = 'deskdb'
@@ -18,11 +18,11 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'aPAcheHel!c0ptEr'
-    HOST = ''
+    HOST = '0.0.0.0'
 
 
 class ProductionConfig(Config):
-    HOST = '0.0.0.0'
+    
 
 
 class StagingConfig(Config):
@@ -34,7 +34,6 @@ class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
     RELOAD = True
-    HOST = '0.0.0.0'
 
 
 class TestingConfig(Config):
@@ -43,4 +42,5 @@ class TestingConfig(Config):
 
 class DockerConfig(Config):
     '''Docker container configuration'''
-    #REMEMBERTO: bind the host to the right env variables
+    DATABASE_URI = os.environ['DATABASE_URI']
+    
