564ba364955e4f318d517bccf9ffa1e381f36641
PI-Victor <irap4fun@gmail.com>
2015-04-13 01:57:14 +0200

    config load and optional config included

--- viewpanel/app.py
+++ viewpanel/app.py
@@ -1,13 +1,28 @@
 import logging
 import os
+import logging
 
 import flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 from .config import config
 from .views import pages
 
+logger = logging.getLogger(__name__)
 
-def app_factory():
+
+def app_factory(config, envfile=''):
     app = flask.Flask(__name__)
+    app.config.from_object(config)
+    logging.debug(envfile)
+    #load aditional configuration if specified
+    if envfile:
+        try:
+            app.config.from_pyfile(envfile)
+        except Exception as e:
+            logging.info('Unable to load config file. Using defaults!', e)
+            pass
+        
+    db = SQLAlchemy(app)
     app.register_blueprint(pages)
     return app
