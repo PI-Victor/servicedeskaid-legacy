5430e5703b59858956dfc6bb36a60838ff8d7c40
PI-Victor <irap4fun@gmail.com>
2015-04-15 00:32:07 +0200

    fixed test with config import

--- test/application_test.py
+++ test/application_test.py
@@ -1,8 +1,11 @@
 #Test application and database settings
 
 from flask import Flask
+
 from servicedeskaid.app import app_factory
+from servicedeskaid.config import config
+
 
 def test_app():
-    app = app_factory()
+    app = app_factory(config.Testing)
     assert isinstance(app, Flask)
