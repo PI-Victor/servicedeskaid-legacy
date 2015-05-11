c3a7e742fca653e47d83c629712aebd3e76cc496
PI-Victor <irap4fun@gmail.com>
2015-05-11 20:21:52 +0200

    temp removed out user verification

--- servicedeskaid/views.py
+++ servicedeskaid/views.py
@@ -6,16 +6,16 @@ from .forms import LoginForm
 pages = flask.Blueprint('pages', __name__)
 
 
-@pages.before_request
-def before_request():
-    flask.g.user = current_user
+#@pages.before_request
+#def before_request():
+#    flask.g.user = current_user
 
 @pages.route('/')
 def index():
-    if not flask.g.user.is_authenticated:
+ #   if not flask.g.user.is_authenticated:
         return flask.render_template('login.html')
     
-    return flask.render_template('wall.html')
+ #   return flask.render_template('wall.html')
     
 @pages.route('/login')
 def login():
