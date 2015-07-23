3d64fc581d51575eebc31f31b9b3189381b90619
PI-Victor <irap4fun@gmail.com>
2015-07-24 01:20:54 +0200

    fixed dependencies and added user controller

--- rest-api.js
+++ rest-api.js
@@ -4,10 +4,9 @@ var express = require('express'),
 
 
 require('application')(api)
-
 require('routes')(api)
 
-var server = api.listen(process.env.PORT || 3030, function(){
+var server = api.listen(process.env.PORT || 3030, process.env.HOST || 'localhost', function(){
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Listening on http://%s:%s'.green, host, port);
